21a60ffd9e624b12b9438d7f2c9ae3bb6d7ea7d0
Ragz-no <70691805+Ragz-no@users.noreply.github.com>
2022-08-31 12:49:14 +0200

    first commit

---
+++ controllers/app.js
@@ -0,0 +1,25 @@
+//Jshint esversion:6
+
+const express require("express");
+const bodyparser = require("body-parser");
+const ejs = require("ejs");
+
+
+const req = require("express/lib/request");
+const res = require("express/lib/response");
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname, "../public"));
+app.set("views", "../views");
+
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/about",(req, res) => {
+    res.render("about");
+})
